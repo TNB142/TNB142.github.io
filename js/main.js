e39093b4531d58fe3d5536a7837f9938c9a3ceb5
@TNB142 <20520142@gm.uit.edu.vn>
2023-08-09 01:57:13 +0700

    upload full

--- js/main.js
+++ js/main.js
@@ -6,3 +6,28 @@ menuButton.addEventListener("click", () => {
   menu.classList.toggle("navbar__open");
   menuButton.classList.toggle("open");
 });
+
+//Show-hidden bib text
+const divContainer1 = document.querySelector("#Show_Bib_code1");
+let isClicked1 = true;
+let showOrHide1 = function () {
+  if (isClicked1) {
+    divContainer1.style.display = "block";
+    isClicked1 = false;
+  } else {
+    divContainer1.style.display = "none";
+    isClicked1 = true;
+  }
+};
+//Show-hidden bib text
+const divContainer2 = document.querySelector("#Show_Bib_code2");
+let isClicked2 = true;
+let showOrHide2 = function () {
+  if (isClicked2) {
+    divContainer2.style.display = "block";
+    isClicked2 = false;
+  } else {
+    divContainer2.style.display = "none";
+    isClicked2 = true;
+  }
+};
